58ac766ffd5b50621f55cb245a032cc85aa3d418
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-01-20 00:00:37 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+const URL = `https://restcountries.com/v3.1/all`;
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchCountries();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Countries Flag</h1>
+      <div className="cards">
+        {countries.map((country) => {
+          return (
+            <div className="cardstyle">
+              <img
+                src={country.flags.png}
+                alt={country.name.common}
+                height={100}
+                width={100}
+              />
+              <h2>{country.name.common}</h2>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
